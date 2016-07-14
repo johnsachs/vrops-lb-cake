583c33f3ee54970e59cf07b5c310f86f44081b25
johnsachs <johnsachs@users.noreply.github.com>
2016-07-14 16:54:30 -0700

    added back in after craziness

---
+++ vrops-lb-cake.pl
@@ -0,0 +1,174 @@
+## vROps LB cake
+## jsachs
+use strict;
+use warnings;
+use POSIX;
+use Cwd;
+
+my %nodes = ();
+my $admin_node = 0;
+my @months = ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+my @info_file = ('status.txt', 'df.txt');
+my ($sec, $min, $hour, $day, $month, $year);
+my $milli = 0;
+my $nodecount = 0;
+my $cwd = getcwd();
+
+opendir (my $cdir, $cwd) or die "can't get cwd: $!";
+my @ext_dirs = grep { /-extracted$||!\.zip$/ } readdir $cdir;
+closedir $cdir;
+
+foreach (@ext_dirs) {
+
+    ## find the admin node ##
+    if (/^(.*)_(\d{13})_(\1)/) {
+        #print "DEBUG: \$_ =>$_<=\n";
+        #print "DEBUG: \$1 =>$1<=\n";
+        #print "DEBUG: \$2 =>$2<=\n";
+        #print "DEBUG: \$3 =>$3<=\n";
+        next unless -d $_;
+        next unless $1 eq $3;
+        $admin_node = $1;
+        $nodes{$1}{'dir'} = "$cwd/$_";
+        $nodes{$1}{'admin'} = 1;
+
+        #print "DEBUG: \$admin_node =>$admin_node<=\n";
+
+        ## extract the timestamp from the directory name
+        ## and convert it to human readable form
+        ## only perform this once (unless there are multime timestamps in
+        ## a single log bundle which should probably not happen)
+        $milli = $2;
+        my $epoch = $milli;
+        $epoch =~ s/\d{3}$//;
+        #print"DEBUG: epoch: =>$epoch<=\n";
+        ($sec, $min, $hour, $day, $month, $year) = (localtime($epoch))[0,1,2,3,4,5];
+        foreach ($hour, $min, $sec) {
+            $_ = "0$_" unless length($_) > 1;
+        }
+
+        print "Log Bundle Time: ".$months[$month]." ".$day.", ".($year+1900)." $hour:$min:$sec ";
+        print strftime("%Z", localtime()) . "\n";
+
+        last;
+    }
+
+}
+
+die "did not find admin node directory!" unless $admin_node;
+
+# Get the non-admin nodes in the cluster
+foreach (@ext_dirs) {
+
+    next if /^($admin_node|cluster)/;
+    if (/^(.*)_($milli)_($admin_node)/) {
+        next unless -d $_;
+        $nodes{$1}{'dir'} = "$cwd/$_";
+        $nodes{$1}{'admin'} = 0;
+    }
+
+}
+
+foreach (sort keys %nodes) {
+    my $nodename = $_;
+    my $nodedir = $nodes{$_}{'dir'};
+    my $sysenv_dir = "$nodedir/sysenv";
+    my $conf_dir = "$nodedir/conf";
+    my $logs_dir = "$nodedir/logs";
+
+    ## do node directory processing
+    print "$nodename\n";
+    my $vrops_version = read_file("$conf_dir/lastbuildversion.txt");
+    $nodes{$nodename}{'version'} = $vrops_version;
+    print "$vrops_version\n";
+
+    # Get node IP
+    $nodes{$nodename}{'ipaddress'} =
+        get_node_ipaddress(read_file("$sysenv_dir/ifconfig.txt"));
+
+    # CPU
+    my $cpu_count = 0;
+    my $cpu_name = 'CPU MODEL NAME NOT FOUND!';
+    my $cpuinfotxt = "$sysenv_dir/cpuInfo.txt";
+    $cpuinfotxt = "$sysenv_dir/cpuinfo.txt" unless -e $cpuinfotxt; 
+    my $cpuinfo = read_file($cpuinfotxt);
+    foreach my $line (split(/\n/, $cpuinfo)) {
+        $cpu_count++ if $line =~ /^processor/;
+        if ($line =~ /^model name\s*: (.*)$/) {
+            $cpu_name = $1;
+        }
+    }
+    print "$cpu_count X $cpu_name\n";
+
+    # RAM
+    my ($mem_total, $mem_free, $swap_total, $swap_free) = (0, 0, 0, 0);
+    my $meminfotxt = "$sysenv_dir/memInfo.txt";
+    $meminfotxt = "$sysenv_dir/meminfo.txt" unless -e $meminfotxt;
+    my $meminfo = read_file($meminfotxt);
+    foreach my $line (split(/\n/, $meminfo)) {
+        if ($line =~ /^MemTotal:\s*(\d+) kB/) {
+            $mem_total = $1;
+        }
+        if ($line =~ /^MemFree:\s*(\d+) kB/) {
+            $mem_free = $1;
+        }
+        if ($line =~ /^SwapTotal:\s*(\d+) kB/) {
+            $swap_total = $1;
+        }
+        if ($line =~ /^SwapFree:\s*(\d+) kB/) {
+            $swap_free = $1;
+        }
+    }
+    # format values to GB and round to 2 places after the decimal
+    foreach ($mem_total, $mem_free, $swap_total, $swap_free) {
+        $_ = $_/1024/1024;
+        $_ = sprintf("%.2f", $_);
+    }
+
+    print "$mem_total GB RAM / $mem_free GB free\n$swap_total GB swap / $swap_free GB free\n\n";
+
+
+    foreach (@info_file) {
+        my $info = read_file("$sysenv_dir/$_");
+        $info =~ s/^(?:.*\n){1,4}//;
+        print $info;
+        print "\n";
+    }
+
+}
+
+# node report
+foreach (sort keys %nodes) {
+    my $ipaddress = $nodes{$_}{'ipaddress'};
+    print "$_ - $ipaddress\n";
+    print $nodes{$_}{'version'} . "\n";
+}
+
+sub read_file {
+    my ($filename) = @_;
+ 
+    open my $in, '<:encoding(UTF-8)', $filename or die "Could not open '$filename' for reading $!";
+    local $/ = undef;
+    my $all = <$in>;
+    close $in;
+ 
+    return $all;
+}
+
+sub get_node_ipaddress {
+    my $ifconfig = shift;
+    my $ipaddress = '';
+    my $record = 0;
+
+    foreach (split(/\n/, $ifconfig)) {
+        $record = 1 if /^eth/;
+        next unless $record;
+        #print "DEBUG: =>$_<=\n";
+        if (/inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
+            $ipaddress = $1;
+            last;
+        }
+    }
+
+    return $ipaddress;
+}
